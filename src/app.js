f77a6c0780771c0d7bf71b7227745668af972fea
Spencer Smith <dontaskspencer@gmail.com>
2018-08-18 22:09:08 -0600

    Working on complex inserts in doc writer

--- src/app.js
+++ src/app.js
@@ -1,28 +1,16 @@
 const PDF = require('pdfkit');
-const fs = require('fs');
 
 const { font } = require('./constants.js');
 
-/**
- * Document
- */
-const doc = new PDF();
-doc.pipe(fs.createWriteStream('output.pdf'));
-
-const { insertHead, insertSub, insertPara } = require('./writer.js')(doc);
+const doc = new PDF({
+  margin: 30,
+});
+const { insertHead, insertSub, insertPara, insertExperience } = require('./writer.js')(doc);
 const { personalInfo, workHistory } = require('./data.json');
 
-doc.addPage({
-  margin: 10,
-})
-
 insertHead(personalInfo.name);
-
 insertSub('EXPERIENCE');
 
-workHistory.map(({ title, company }) => {
-  insertPara(title, { bold: true });
-  insertPara(company.name);
-});
+workHistory.map(insertExperience);
 
 doc.end()
--- src/writer.js
+++ src/writer.js
@@ -1,10 +1,17 @@
 const { font } = require('./constants.js');
+const fs = require('fs');
 
-module.exports = (doc) => {
+const setup = (doc) => {
+  doc.pipe(fs.createWriteStream('output.pdf'));
+  doc.registerFont('semi-bold', 'src/fonts/FiraSans-SemiBold.ttf');
+  doc.registerFont('regular', 'src/fonts/FiraSans-Regular.ttf');
+};
+
+const simple = (doc) => {
   return {
     insertHead: (text) => {
       doc
-        .font('src/fonts/FiraSans-SemiBold.ttf')
+        .font('semi-bold')
         .fontSize(font.header)
         .text(text)
 
@@ -12,7 +19,7 @@ module.exports = (doc) => {
     },
     insertSub: (text) => {
       doc
-        .font('src/fonts/FiraSans-SemiBold.ttf')
+        .font('semi-bold')
         .fontSize(font.subheader)
         .text(text, {
           characterSpacing: 1,
@@ -23,11 +30,30 @@ module.exports = (doc) => {
     insertPara: (text, options = {}) => {
       const { bold } = options;
       doc
-        .font(bold ? 'src/fonts/FiraSans-SemiBold.ttf' : 'src/fonts/FiraSans-Regular.ttf')
+        .font(bold ? 'semi-bold' : 'regular')
         .fontSize(font.normal)
         .text(text)
 
         .moveDown(bold ? 0.5 : 0.75)
     }
+  };
+};
+
+const complex = (doc) => {
+  const simp = simple(doc);
+  return {
+    insertExperience: (item) => {
+      const { title, company } = item;
+      simp.insertPara(title, { bold: true });
+      simp.insertPara(company.name);
+    }
+  };
+};
+
+module.exports = (doc) => {
+  setup(doc);
+  return {
+    ...simple(doc),
+    ...complex(doc),
   };
 };
